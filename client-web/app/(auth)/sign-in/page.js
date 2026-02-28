886ff9bc4a423d979824e6eb3aa090571fcc5c32
Rahat <rrahatislamm@gmail.com>
2026-03-01 03:17:51 +0530

    initialize next.js

---
+++ client-web/app/(auth)/sign-in/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome back...</h1>
+      <p>Sign in with your email and password</p>
+    </div>
+  )
+}
+
+export default page
