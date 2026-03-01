c4214fda7612af54fd02fa244aaefe1cc177ff1e
Rahat <rrahatislamm@gmail.com>
2026-03-01 19:54:24 +0530

    did the sign-in page

--- client-web/app/(auth)/sign-in/page.js
+++ client-web/app/(auth)/sign-in/page.js
@@ -1,12 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 import React from 'react'
 
-const page = () => {
+const SignIn = () => {
   return (
-    <div>
-      <h1>Welcome back...</h1>
-      <p>Sign in with your email and password</p>
+    <div className="flex min-h-screen items-center justify-center ">
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+          <CardDescription>
+            Sign up with your email and password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <Input type="email" placeholder="Email" required />
+            <Input type="password" placeholder="Password" required/>
+            <Input type="password" placeholder="Confirm Password" required/>
+            <Button type="submit">Sign In</Button>
+          </form>
+         <CardDescription className={`mt-4`}>Don't have an account? <Link className='text-blue-400' href="/sign-up">Sign Up</Link></CardDescription>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default page
+export default SignIn
--- client-web/app/(auth)/sign-up/page.js
+++ client-web/app/(auth)/sign-up/page.js
@@ -1,11 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const SignUp = () => {
+  const handleSignUp = async () => {}
   return (
-    <div>
-      <h1>Sign up</h1>
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>
+            Sign up with your email and password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <Input type="password" placeholder="Confirm Password" />
+            <Button type="submit">Sign up</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default SignUp
